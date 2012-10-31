08ffa09f87c9133a54fac9e36ebcc1da90815dfc
hesar <hesar@tlen.pl>
2012-10-31 09:56:41 +0100

    Update lib/Phergie/Plugin/CopyCommand.php
    add .slap support

--- lib/Phergie/Plugin/CopyCommand.php
+++ lib/Phergie/Plugin/CopyCommand.php
@@ -67,7 +67,11 @@ class Phergie_Plugin_CopyCommand extends Phergie_Plugin_Abstract
      */
     public function onPrivmsg() {
 //        var_dump(strpos($this->event->getArgument(1),'.hl'));
-        if($this->getConnection()->getHost() == $this->hostFrom && $this->event->getSource() == $this->channelFrom && $this->hasSufficientPrivileges($this->event->getNick(),$this->event->getSource()) && strpos($this->event->getArgument(1),'.hl') !== false)
+        if($this->getConnection()->getHost() == $this->hostFrom && 
+            $this->event->getSource() == $this->channelFrom && 
+            $this->hasSufficientPrivileges($this->event->getNick(),$this->event->getSource()) && 
+            strpos($this->event->getArgument(1),'.slap') !== false &&
+            strpos($this->event->getArgument(1),'.hl') !== false)
           {
               $this->startListening = true;
               $this->slap = true;
